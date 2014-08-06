c4d627098759be4f70435d865458142b426e4e0f
Chris Barker <cbarker@puppetlabs.com>
2014-08-06 08:13:14 -0700

    updates readme

---
+++ README.md
@@ -0,0 +1,5 @@
+Simple script to create nodes in eso environment
+
+./creatnodes.sh $numberofnodes $sshkeytouse
+
+This does not assign public ips to the nodes
--- createnodes.sh
+++ createnodes.sh
@@ -3,8 +3,7 @@
 aws ec2 run-instances --image-id ami-a53b4895 \
   --count ${1} \
   --instance-type t1.micro \
-  --key-name chrisbarker \
+  --key-name ${2} \
   --security-group-ids sg-35199c50 \
   --subnet-id subnet-60ca2317 \
-  --user-data file://userdata.txt \
-  --associate-public-ip-address
+  --user-data file://userdata.txt
